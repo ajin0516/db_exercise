339aad43996f465e96f861218f7154b3f1672ca4
ajin0516 <dkwls7432@naver.com>
2022-10-18 10:43:24 +0900

    자바 mysql 연결

--- src/main/java/dbexercise/UserDao.java
+++ src/main/java/dbexercise/UserDao.java
@@ -4,7 +4,7 @@ import dbexercise.domain.User;
 
 import java.sql.*;
 import java.util.Map;
-
+//5. 종료는 역순으로
 public class UserDao {
     public void add() throws SQLException, ClassNotFoundException {
         Map<String, String> env = System.getenv();
@@ -12,14 +12,16 @@ public class UserDao {
         String dbUser = env.get("DB_USER");
         String dbPassword = env.get("DB_PASSWORD");
 
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection  conn = DriverManager.getConnection(dbHost,dbUser,dbPassword);  // db 연9
-        PreparedStatement ps = conn.prepareStatement("INSERT INTO users(id, name, password) VALUES(?, ?, ?)");
-        ps.setString(1,"2");
+        Class.forName("com.mysql.cj.jdbc.Driver"); // 1. driver loading
+        Connection conn = DriverManager.getConnection(dbHost,dbUser,dbPassword);  // 2. 1번을 해야 연결됨. db 연결
+        PreparedStatement ps = conn.prepareStatement("INSERT INTO users(id, name, password) VALUES(?, ?, ?)"); // 3-1. sql실행 준비 완료/문장 작성
+        ps.setString(1,"2"); // 3-2. 어떤 값이 들어갈지 알려줘야함
         ps.setString(2,"ajin2");
         ps.setString(3,"1234");
 
-        ps.executeUpdate();
+        ps.executeUpdate(); // 4. SQL 실행하라
+
+        // 5. DB 연결 종료 : 역순으로 종료
         ps.close();
         conn.close();
     }
@@ -33,8 +35,9 @@ public class UserDao {
         Connection  conn = DriverManager.getConnection(dbHost,dbUser,dbPassword);
         PreparedStatement ps = conn.prepareStatement("SELECT id, name, password FROM users WHERE id = ?");
         ps.setString(1,id);
-        ResultSet rs = ps.executeQuery();
-        rs.next();
+        ResultSet rs = ps.executeQuery(); // 4. (select문에 resultset, executeQuery 사용 - 주소값을 참조하고 있다고 생각하기) SQL 실행한 값 넘겨주기
+        System.out.println(rs + " 삽입 성공");
+        rs.next();  // 첫번째는 간 다음 다음으로 내려가라(row)
         User user = new User(rs.getString("id"), rs.getString("name"),
                 rs.getString("password"));
         rs.close();
